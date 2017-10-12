ac2aeffd17cdfc974665641981f810d312fc37b8
santRaju <santnagaraj@deloitte.com>
2017-10-12 15:38:11 +0530

    issue fix

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ import lib from './lib/ci';
 
 const WEBHOOK_PATH = '/webhook';
 const SECRET = 'secret';
-const BUILD_PATH = '/root/squad-web';
+const BUILD_PATH = '/Users/santhoshraju/Documents/squad-web';
 
 lib.initialize(WEBHOOK_PATH, SECRET, BUILD_PATH);
