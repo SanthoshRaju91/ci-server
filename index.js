92c444405e73d47bd65922f3076a77b4aec48426
santRaju <santnagaraj@deloitte.com>
2017-10-12 15:04:00 +0530

    adding secret key

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import lib from './lib/ci';
 
 const WEBHOOK_PATH = '/webhook';
-const SECRET = '';
+const SECRET = 'secret';
 const BUILD_PATH = '/root/squad-web';
 
 lib.initialize(WEBHOOK_PATH, SECRET, BUILD_PATH);
