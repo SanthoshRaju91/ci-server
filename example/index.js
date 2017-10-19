3a222c3988d414a2fe00075b43efb8694101402f
santRaju <santnagaraj@deloitte.com>
2017-10-19 14:04:48 +0530

    Library commit

---
+++ example/index.js
@@ -0,0 +1,30 @@
+import CI from './lib/ci';
+
+const WEBHOOK_PATH = '/webhook';
+const SECRET = 'secret';
+const BUILD_PATH = '/Users/santhoshraju/Documents/apollo-mongo';
+
+const config = {
+  WEBHOOK_PATH,
+  SECRET,
+  BUILD_PATH
+};
+
+const ciServer = new CI(config);
+
+ciServer.start(
+  (err, port) => {
+    if (err) {
+      console.log(`Error on port ${port}, ${err}`);
+    } else {
+      console.log(`Webhook listening on port: ${port}`);
+    }
+  },
+  (err, status) => {
+    if (err) {
+      console.log(`Error in deployment ${err}`);
+    } else {
+      console.log(`Deployment completed`);
+    }
+  }
+);
--- index.js
+++
@@ -1,7 +0,0 @@
-import lib from './lib/ci';
-
-const WEBHOOK_PATH = '/webhook';
-const SECRET = 'secret';
-const BUILD_PATH = '/root/squad-web';
-
-lib.initialize(WEBHOOK_PATH, SECRET, BUILD_PATH);
